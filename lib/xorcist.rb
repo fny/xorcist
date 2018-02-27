c3f9e8d327e60bc552c919173e3b63c0bba12a18
Faraz Yashar <faraz.yashar@gmail.com>
2018-02-26 19:04:11 -0500

    Prevent Rubinius from modifying frozen strings

--- lib/xorcist.rb
+++ lib/xorcist.rb
@@ -12,4 +12,18 @@ module Xorcist
   def xor(x, y)
     xor!(x.dup, y)
   end
+
+  # rb_str_modify isn't blowing up for frozen strings under Rubinius, so we'll
+  # hack in a fix here
+  if RUBY_ENGINE == 'rbx'
+    alias_method :_xor!, :xor!
+    remove_method :xor!
+    def xor!(x, y)
+      if x.frozen?
+        raise(RuntimeError, "can't modify frozen String")
+      else
+        _xor!(x, y)
+      end
+    end
+  end
 end
