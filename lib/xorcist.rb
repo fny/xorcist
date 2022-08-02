ef8e923d9991939d76faff88e1348b00ac5dfb97
Faraz Yashar <faraz.yashar@gmail.com>
2022-08-02 16:13:13 -0400

    Depend on autoload for older JRuby versions

--- lib/xorcist.rb
+++ lib/xorcist.rb
@@ -5,8 +5,6 @@ if RUBY_ENGINE == 'jruby'
   require 'xorcist.jar'
   if JRuby.respond_to?(:load_ext)
     JRuby::Util.load_ext('fny.xorcist.XorcistLibrary')
-  else
-    fny.xorcist.XorcistLibrary.new.load(JRuby.runtime, false) # jruby < 9.2
   end
 else
   require 'xorcist/xorcist'
@@ -16,19 +14,5 @@ module Xorcist
   module_function
   def xor(x, y)
     xor!(x.dup, y)
-  end
-
-  # rb_str_modify isn't blowing up for frozen strings under Rubinius, so we'll
-  # hack in a fix here
-  if RUBY_ENGINE == 'rbx'
-    alias_method :_xor!, :xor!
-    remove_method :xor!
-    def xor!(x, y)
-      if x.frozen?
-        raise(RuntimeError, "can't modify frozen String")
-      else
-        _xor!(x, y)
-      end
-    end
   end
 end
